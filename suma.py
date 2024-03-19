728ca289feadc3a0545fe9a5e27f2766486086e5
IvanPz01 <ivanciito_999@hotmail.com>
2024-03-19 13:47:27 -0300

    correccion de errores

--- suma.py
+++ suma.py
@@ -2,7 +2,7 @@ numero = int(input("Escribe un número: "))
 numero2 = int(input("Escribe otro número: "))
 
 def suma(numero, numero2):
-    return numero + numero2
+    return numero - (-numero2)
 
 resultado = suma(numero, numero2)
 print("El resultado de la suma es: ", resultado)
